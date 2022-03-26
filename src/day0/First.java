a2d808b4e20f115c0801b4e1371cc167e9b2d58e
Azamat1KZ <98862639+Azamat1KZ@users.noreply.github.com>
2022-03-26 16:14:05 -0400

    updated First for the third time

--- src/day0/First.java
+++ src/day0/First.java
@@ -5,7 +5,6 @@ public class First {
         System.out.println("java");
         System.out.println("working...");
     }
-    //TODO: Work on this tomorrow, add some cool code
     //extra line 1
     //extra line 2
     //extra line 3
