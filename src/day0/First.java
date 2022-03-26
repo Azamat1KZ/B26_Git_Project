80bb1f8706d002093aa404de4069cbdc6fe87017
Azamat1KZ <98862639+Azamat1KZ@users.noreply.github.com>
2022-03-26 16:10:24 -0400

    Wrote the code for First class

--- src/day0/First.java
+++ src/day0/First.java
@@ -1,6 +1,10 @@
 package day0;
 
 public class First {
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("working...");
+    }
     //TODO: Work on this tomorrow, add some cool code
     //extra line 1
     //extra line 2
